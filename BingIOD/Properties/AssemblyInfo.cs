c35831b467821d1f4fa874eda5b6ab3651abd268
James Geater <jgeater@outlook.com>
2019-03-10 09:40:38 -0400

    Fixed version info

--- BingIOD/Properties/AssemblyInfo.cs
+++ BingIOD/Properties/AssemblyInfo.cs
@@ -6,11 +6,11 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("BingIOD")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("Downloads last few bing images of the day")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("Allstate Insurance Company")]
+[assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("BingIOD")]
-[assembly: AssemblyCopyright("Copyright © Allstate Insurance Company 2016")]
+[assembly: AssemblyCopyright("")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("1.2019.03.10")]
+[assembly: AssemblyFileVersion("1.2019.03.10")]
