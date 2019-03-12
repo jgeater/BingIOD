9c323d9991a3048cbe62d2e13bec2397b34145ef
James Geater <jgeater@outlook.com>
2019-03-11 20:08:52 -0400

    minor update
    added
    error log
    dont DL file if it already exists

--- BingIOD/Program.cs
+++ BingIOD/Program.cs
@@ -5,6 +5,10 @@ using System.Net.Http;
 using System.Threading.Tasks;
 using System.Net;
 using System.IO;
+using System.Threading;
+using System.Linq;
+using System.Management;
+
 
 namespace BingIOD
 {
@@ -12,6 +16,18 @@ namespace BingIOD
     {
         static void Main(string[] args)
         {
+            //add something to check internet connection
+
+            bool isconnected = CheckForInternetConnection();
+
+            if (isconnected == false)
+            {
+                Console.WriteLine("No Internet connection found");
+                Console.WriteLine("Connect to the internet and run the application again");
+                Thread.Sleep(5000);
+                Environment.Exit(1);
+            }
+
             BingURL();
             //im using console.readkey to keep thing open whil downloading the files
             //I need to fivure out how to use a task to make it wait
@@ -31,7 +47,7 @@ namespace BingIOD
             strJSONString = await response.Content.ReadAsStringAsync();
 
             //Split strJSONString on ,
-            char[] delimiterChars = {','};
+            char[] delimiterChars = { ',' };
             string[] imgs = strJSONString.Split(delimiterChars);
 
             //now that it is split look at each line for a file name and download it
@@ -40,20 +56,20 @@ namespace BingIOD
                 string s2 = "jpg";
                 bool b = s1.Contains(s2);
                 if (b)
-                    {
+                {
                     //look for the JPS and it's path and trun it into a URL
                     string s3 = s1.Replace(":", "");
-                    s3 = s3.Replace("url","https://bing.com");
+                    s3 = s3.Replace("url", "https://bing.com");
                     s3 = s3.Replace("\"", "");
                     Console.WriteLine(s3);
 
                     //now pull the file name out of the URL
                     String fileName = Path.GetFileName(s3);
-                    fileName= fileName.Substring(fileName.IndexOf('.') + 1);
+                    fileName = fileName.Substring(fileName.IndexOf('.') + 1);
                     int index = fileName.IndexOf("&");
-                    fileName =fileName.Substring(0, index);
-                    Console.WriteLine("Filename = "+fileName);
-                    
+                    fileName = fileName.Substring(0, index);
+                    Console.WriteLine("Filename = " + fileName);
+
                     //the URL is now stored in s3 do I need to download it
                     Console.WriteLine("Downloading the file {0}", fileName);
                     try
@@ -61,13 +77,31 @@ namespace BingIOD
                         string DlDir = Environment.GetFolderPath(Environment.SpecialFolder.MyPictures);
                         Console.WriteLine(DlDir);
                         WebClient webClient = new WebClient();
-                        webClient.DownloadFile(s3, DlDir + @"\" + fileName);
-                        Console.WriteLine("Download Completed");
+                        string DLfile = DlDir + @"\" + fileName;
+                        if (!File.Exists(DLfile))
+                        {
+                            webClient.DownloadFile(s3, DlDir + @"\" + fileName);
+                            Console.WriteLine("Download Completed");
+                            Thread.Sleep(2000);
+                        }
+                        if (File.Exists(DLfile))
+                        {
+                            Console.WriteLine("File already exists!");
+                            Thread.Sleep(1000);
+                        }
 
                     }
-                    catch (Exception)
+                    catch (Exception ex)
                     {
                         Console.WriteLine("Error Downloading File");
+                        string DefaultLogFilePath = Environment.GetFolderPath(Environment.SpecialFolder.UserProfile);
+                        using (StreamWriter w = File.AppendText(DefaultLogFilePath + "BingIOD_RR.log"))
+                        {
+                            Log("Error downloading file: " + fileName, w);
+                            Log("URL: " + s3, w);
+                            Log(ex.ToString(), w);
+                        }
+
                         //Console.ReadKey();
 
                     }
@@ -75,9 +109,32 @@ namespace BingIOD
                 }
 
             }
+            Thread.Sleep(5000);
 
             Environment.Exit(0);
             //Console.ReadKey();
+
+        }
+        public static bool CheckForInternetConnection()
+        {
+            try
+            {
+                using (var client = new WebClient())
+                using (client.OpenRead("http://www.bing.com/HPImageArchive.aspx?format=js&idx=0&n={0}&mkt={1}"))
+                {
+                    return true;
+                }
+            }
+            catch
+            {
+                return false;
+            }
+        }
+
+        //logging
+        public static void Log(string logMessage, TextWriter w)
+        {
+            w.WriteLine("{0}: {1}: {2}", DateTime.Now.ToLongTimeString(), DateTime.Now.ToLongDateString(), logMessage);
         }
 
     }
